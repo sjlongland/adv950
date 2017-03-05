270fd28af753f1c36b05a41556c64ef5e1adf7a5
Stuart Longland <me@vk4msl.id.au>
2017-03-05 11:24:29 +1000

    serial_core: Drop refrences to `tty_locked`
    As per 906cbe1364d94da7cbf74c1d05e3e78b2883f661.

--- 3.x/serial_core.c
+++ 3.x/serial_core.c
@@ -1244,8 +1244,6 @@ static void uart_close(struct tty_struct *tty, struct file *filp)
 	struct uart_port *uport;
 	unsigned long flags;
 
-	BUG_ON(!tty_locked());
-
 	if (!state)
 		return;
 
@@ -1426,7 +1424,6 @@ static void uart_hangup(struct tty_struct *tty)
 	struct tty_port *port = &state->port;
 	unsigned long flags;
 
-	BUG_ON(!tty_locked());
 	pr_debug("uart_hangup(%d)\n", state->uart_port->line);
 
 	mutex_lock(&port->mutex);
@@ -1513,7 +1510,6 @@ static int uart_open(struct tty_struct *tty, struct file *filp)
 	struct tty_port *port;
 	int retval, line = tty->index;
 
-	BUG_ON(!tty_locked());
 	pr_debug("uart_open(%d) called\n", line);
 
 	/*
